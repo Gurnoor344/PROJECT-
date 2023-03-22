3a918c7d3075ca01227ad89fb42bf2e3b08875d9
Gurnoor344 <124993584+Gurnoor344@users.noreply.github.com>
2023-03-22 11:40:07 +0530

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,7 @@
+DEFINATION 
+The tic-tac-toe game is played on a 3x3 grid the game is played by two players, who take turns. The first player marks moves with a circle, the second with a cross. The player who has formed a horizontal, vertical, or diag-onal sequence of three marks wins. Your program should draw the game board, ask the user for the coordinates of the next mark, change the players after every successful move, and pronounce the winner.
+SAMPLE TO PLAY TIC TAC TOE
+Have the first player go first. 
+Have the second player go second. 
+Keep alternating moves until one of the players has drawn a row of three symbols or until no one can win.
+Keep practicing.
