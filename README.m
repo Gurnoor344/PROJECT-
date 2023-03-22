2dffecdc772ae968a175b8fbd0f74a4bcb2c0a36
Gurnoor344 <124993584+Gurnoor344@users.noreply.github.com>
2023-03-22 11:42:20 +0530

    Update README.m

--- README.m
+++ README.m
@@ -4,9 +4,3 @@ The first player marks moves with a circle, the second with a cross.
 The player who has formed a horizontal, vertical, or diag-onal sequence of three marks wins.
 Your program should draw the game board, ask the user for the coordinates of the next mark, change the players after every successful move, and pronounce the winner.
 
-SAMPLE :- 
-TO PLAY TIC TAC TOE
-Have the first player go first. 
-Have the second player go second. 
-Keep alternating moves until one of the players has drawn a row of three symbols or until no one can win.
-Keep practicing.
